d846d8b0ac42ac539741375d88ea36d963313ca5
Sakshi Thodsare <thodsare.sakshi@kgce.edu.in>
2024-11-12 18:51:08 +0530

    added

---
+++ Assignment_6/Q2.js
@@ -0,0 +1,20 @@
+let num1 = 50;
+let num2 = 29;
+
+
+// addition
+console.log(num1 + num2);
+// substaction
+console.log(num1 - num2);
+// multiplication
+console.log(num1 * num2);
+// division
+console.log(num1 / num2);
+// greater then
+console.log(num1 > num2);
+// less then
+console.log(num1 < num2);
+// double equal to
+console.log(num1 == num2);
+// triple equal to
+console.log(num1 === num2);
