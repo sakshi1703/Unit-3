7d3da3a6fab4e3c7817d9d0afdd841c5b7dd6433
Sakshi Thodsare <thodsare.sakshi@kgce.edu.in>
2024-11-12 19:49:38 +0530

    added

---
+++ Assignment_6/Q3.js
@@ -0,0 +1,14 @@
+numValue = 20;
+stringValue = "42";
+
+let res = Number(stringValue) ;
+let result = numValue + res
+console.log(result);
+
+
+if(result > 100){
+    console.log("The result is greater then 100");
+}
+else{
+    console.log("result is less then 100")
+}
